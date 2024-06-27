478bfe3b7d8d146eb246262694c6b4bb9baba7e9
Brenda J. Freire da Costa <brendajoycefreire@gmail.com>
2024-06-27 12:03:25 -0300

    operadores condicionais

---
+++ condicionais/exemplo02.py
@@ -0,0 +1,5 @@
+idade = 10
+if idade >= 18:
+    print('Você é maior de idade! Acesso liberado.')
+else:
+    print("Você é menor de idade. Acesso negado.")
---
+++ condicionais/exemplo03.py
@@ -0,0 +1,7 @@
+temperatura = 25
+if temperatura > 25:
+    print("Está calor, vamos para a praia!")
+elif temperatura < 20:
+    print("Está frio, vamo ficar em casa codando")
+else:
+    print("A temperatura está agradável, vai dar uma volta")
