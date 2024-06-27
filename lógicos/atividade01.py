e0ef17dca245485813be26690ced90e88b82fa69
Brenda J. Freire da Costa <brendajoycefreire@gmail.com>
2024-06-27 12:12:49 -0300

    operadores lógicos

---
+++ lógicos/atividade01.py
@@ -0,0 +1,5 @@
+nota = int(input('Insira a sua nota: '))
+if nota >= 7:
+    print('Você foi aprovada!')
+else:
+    print('Você foi reprovada')
---
+++ lógicos/atividade02.py
@@ -0,0 +1,6 @@
+nota = float(input('Digite a sua nota: '))
+presenca = float(input('Digite a sua presença: '))
+if nota >= 7 and presenca >= 75:
+    print('Aprovada!')
+else:
+    print('Reprovada')
